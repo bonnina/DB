f2e457e580c8c94f7c4081d92b21780b5a87d0db
Nina Bondarchuk <nibo@simcorp.com>
2021-07-04 17:32:53 +0300

    Update markup and styles

---
+++ Pages/Trades/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DB.Models;
+
+namespace DB.Pages.Trades
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DB.Data.Context _context;
+
+        public IndexModel(DB.Data.Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Trade> Trade { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Trade = await _context.Trades.ToListAsync();
+        }
+    }
+}
---
+++ Pages/Workers/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DB.Models;
+
+namespace DB.Pages.Workers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DB.Data.Context _context;
+
+        public IndexModel(DB.Data.Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Worker> Worker { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Worker = await _context.Workers.ToListAsync();
+        }
+    }
+}
